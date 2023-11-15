232cba1743b306acf8bc4a20ef632e9e418fe95c
Ingrid Anjos <ingrid_anjos@hotmail.com>
2023-11-15 20:06:48 -0300

    feat: create form structure and style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FormsComponent } from './components/forms/forms.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FormsComponent
   ],
   imports: [
     BrowserModule
