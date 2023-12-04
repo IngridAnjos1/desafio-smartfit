7df1cfa50e15399105047ad5dac09dc2bdd33440
Ingrid Anjos <ingrid_anjos@hotmail.com>
2023-12-04 20:39:10 -0300

    feat: create unit cards list

---
+++ src/app/components/cards-list/cards-list.component.ts
@@ -0,0 +1,18 @@
+import { GetUnitsService } from 'src/app/services/get-units.service';
+import { Component, Input, OnInit } from '@angular/core';
+import { Location } from 'src/app/types/location.interface';
+
+@Component({
+  selector: 'app-cards-list',
+  templateUrl: './cards-list.component.html',
+  styleUrls: ['./cards-list.component.scss']
+})
+export class CardsListComponent implements OnInit{
+  @Input() unitsList: Location[] = [];
+
+  constructor () {}
+
+    ngOnInit(): void {  
+      console.log(this.unitsList)  
+    }
+}
